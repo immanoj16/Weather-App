3d515c13027e998f0e587d2628dbe9301635f707
immanoj16 <mmaharana14@gmail.com>
2017-08-19 18:33:01 +0530

    Add: google map instead of city names

--- src/js/containers/WeatherList.js
+++ src/js/containers/WeatherList.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import _ from 'lodash'
 
 import Chart from '../components/chart'
+import GoogleMap from '../components/GoogleMap'
 
 class WeatherList extends Component {
 
@@ -12,10 +13,11 @@ class WeatherList extends Component {
     const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273.15)
     const pressures = cityData.list.map(weather => weather.main.pressure)
     const humidities = cityData.list.map(weather => weather.main.humidity)
+    const { lat, lon } = cityData.city.coord
 
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lat={lat} lon={lon} /></td>
         <td><Chart data={temps} color="orange" unit="C"/></td>
         <td><Chart data={pressures} color="green" unit="hPa"/></td>
         <td><Chart data={humidities} color="black" unit="%"/></td>
